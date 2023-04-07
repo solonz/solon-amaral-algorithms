7e915da1ab9a04bda97fe61a11f8a25f31112cd8
Solon <amaral.solon@hotmail.com>
2023-04-07 17:20:16 -0300

    Reqs 1 e 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("john", "doe")
+
+    with pytest.raises(TypeError):
+        encrypt_message(0, 1)
+
+    assert encrypt_message("mouse", 10) == "esuom"
+
+    assert encrypt_message("mouse", 4) == "e_suom"
+
+    assert encrypt_message("mouse", 3) == "uom_es"
